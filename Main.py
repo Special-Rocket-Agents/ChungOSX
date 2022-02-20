8e68fd09f40740efcf68bb07d0ae076d9c5cfd0b
lemane291 <xyyl691@gmail.com>
2022-02-20 20:53:59 +0400

    trol

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 import logging
 import os
 import json
@@ -149,7 +150,7 @@ while not shit:
         elif msg.startswith("chung --"):
             if msg[8:] == "discord":
                 webbrowser.open_new_tab("https://discord.gg/mz3HmzP5ac")
-            elif msg[8:] == 'version':
+            elif msg[8:] == "version":
                 print("Nug ChungOS Version - 1.0\nChungOS version - 0.0.1")
 
         elif msg == "run-lua":
