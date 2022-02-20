0c7f1d4383ff1eb8931e5aa4d0ae80942edbfea7
Arezalgamer89 <86628069+ArezalGame89@users.noreply.github.com>
2022-02-20 20:19:48 +0330

    Better not forget the trusty bad boy
    You forgot about the Brug Bootloader

--- Main.py
+++ Main.py
@@ -150,7 +150,7 @@ while not shit:
             if msg[8:] == "discord":
                 webbrowser.open_new_tab("https://discord.gg/mz3HmzP5ac")
             elif msg[8:] == 'version':
-                print("Nug ChungOS Version - 1.0\nChungOS version - 0.0.1")
+                print("Nug ChungOS Version - 1.0\nChungOS version - 0.0.1\nBrug Bootloader v0.1")
 
         elif msg == "run-lua":
             for i in os.listdir("assets/preload/raw_scripts/"):
