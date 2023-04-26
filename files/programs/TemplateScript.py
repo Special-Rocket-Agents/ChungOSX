ceb614845e83c2edcad89dad82d6e7e187cdce1a
Arezalgamer89 <aradytfa@gmail.com>
2023-04-26 20:57:42 +0430

    ChungOS/Programs TemplateScript
    Made a template script to demonstrate that you can make programs in it too without breaking it unintentionally.

---
+++ files/programs/TemplateScript.py
@@ -0,0 +1,6 @@
+print("This example demonstrates the development of ChungOS Programs.")
+input("PRESS ENTER")
+import time
+print("Look, it even works with libraries!")
+time.sleep(2)
+print("Look, it even works with libraries like time!")
